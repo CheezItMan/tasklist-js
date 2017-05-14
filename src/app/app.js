6eba7e0216a84c6a1624ed44d1497b78321b8ae2
Chris <chris@adadevelopersacademy.org>
2017-05-14 14:08:24 -0700

    Added .jshintrc

--- src/app/app.js
+++ src/app/app.js
@@ -4,6 +4,8 @@ import '../css/_settings.scss';
 import '../css/foundation.css';
 // Load jQuery
 import $ from 'jquery';
+import Backbone from 'backbone';
+import _ from 'underscore';
 
 // ready to go
 $(document).ready(function() {
