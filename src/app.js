7d8320ea4bb562facfda83c7a58aa1b4b73d2324
Chris <chris@adadevelopersacademy.org>
2017-05-14 20:59:01 -0700

    Added Task to app.js, it reports to the console.

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ require('./css/styles.css');
 // Import jQuery
 import $ from 'jquery';
 import _ from 'underscore';
+import Task from './models/task';
 
 // ready to go
 $(document).ready(function() {
 
-  $('section.main-content').append('<p>Hello World!</p>');
+  var my_task = new Task({
+    title: "Create a model",
+    completed: true
+  });
+
+  console.log(my_task.get("title") +  " is completed: " + my_task.get("completed"));
+
 
 });
