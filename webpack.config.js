738de937eac2a6098d06dbb3eae93d58fe6ab8ee
Chris <chris@adadevelopersacademy.org>
2017-05-15 00:11:18 -0700

    Going back a version.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HTMLWebpackPlugin = require('html-webpack-plugin');
 
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
 const DEVELOPMENT = process.env.NODE_ENV === 'development';
+
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
-var entry = PRODUCTION ? './src/app.js'  :
-              ['./src/app.js',
+var entry = PRODUCTION ? './src/app/app.js'  :
+              ['./src/app/app.js',
                 'webpack/hot/dev-server',
                 'webpack-dev-server/client?http://localhost:8080'
               ];
-
 var plugins = PRODUCTION ? [
                   new webpack.LoaderOptionsPlugin({ minimize: false, debug: false }),
                   new ExtractTextPlugin('style-[contenthash:10].css'),
@@ -20,15 +20,16 @@ var plugins = PRODUCTION ? [
                   })
               ]
               : [
-                new webpack.HotModuleReplacementPlugin()
-                ];
+                new webpack.HotModuleReplacementPlugin(),
+                new ExtractTextPlugin('styles.scss')
+              ];
+
 plugins.push(
               new webpack.DefinePlugin({
-            		DEVELOPMENT: JSON.stringify(DEVELOPMENT),
-            		PRODUCTION: JSON.stringify(PRODUCTION)
-            	})
+                        DEVELOPMENT: JSON.stringify(DEVELOPMENT),
+                        PRODUCTION: JSON.stringify(PRODUCTION)
+                })
 );
-
 plugins.push (
               new webpack.ProvidePlugin({
                 "$": 'jquery',
@@ -40,10 +41,10 @@ plugins.push (
 
 const cssIdentifier = PRODUCTION ? '[hash:base64:10]' : '[path][name]---[local]';
 
-const cssLoader = PRODUCTION  ?	ExtractTextPlugin.extract({
-			loader: 'css-loader?minimize&localIdentName=' + cssIdentifier
-		})
-	: 	['style-loader', 'css-loader?localIdentName=' + cssIdentifier];
+const cssLoader = PRODUCTION    ?       ExtractTextPlugin.extract({
+                        loader: 'css-loader?minimize&localIdentName=' + cssIdentifier
+                })
+        :       ['style-loader', 'css-loader?localIdentName=' + cssIdentifier];
 
 
 module.exports = {
@@ -51,15 +52,15 @@ module.exports = {
   entry: entry,
   plugins: plugins,
   output: {
-		path: path.join(__dirname, 'dist'),
-		publicPath: PRODUCTION ? '/' : '/dist/',
-		filename: PRODUCTION ? 'bundle.[hash:12].min.js' : 'bundle.js'
-	},
+                path: path.join(__dirname, 'dist'),
+                publicPath: PRODUCTION ? '/' : '/dist/',
+                filename: PRODUCTION ? 'bundle.[hash:12].min.js' : 'bundle.js'
+        },
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel-loader'],
-      exclude: /node_modules/
+     exclude: /node_modules/
     },
     {
       test: /\.(png|jpg|gif)$/,
@@ -68,9 +69,9 @@ module.exports = {
 
     },
     {
-			test: /\.(css|scss)$/,
-			loaders: cssLoader,
-			exclude: /node_modules/
-		}
+                        test: /\.(css|scss)$/,
+                        loaders: cssLoader,
+                        exclude: /node_modules/
+                }
   ]}
 };
