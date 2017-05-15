dcecda86451a23400c1dd59b223151496c3f8aff
Chris <chris@adadevelopersacademy.org>
2017-05-15 11:05:47 -0700

    removed not working template loader.

--- webpack.config.js
+++ webpack.config.js
@@ -72,14 +72,6 @@ module.exports = {
 			test: /\.(css|scss)$/,
 			loaders: cssLoader,
 			exclude: /node_modules/
-		},
-    {
-      test: /\.html$/,
-      loader: "underscore-template-loader",
-      exclude: /node_modules/,
-      query: {
-        engine: 'lodash',
-      }
-    }
+		}
   ]}
 };
