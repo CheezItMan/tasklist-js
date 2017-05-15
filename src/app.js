5bf5b4f430934a8e77c86f9dd2f364d776225bdd
Chris <chris@adadevelopersacademy.org>
2017-05-15 09:32:09 -0700

    Moving render function to fix templating errors on deployment.

--- src/app.js
+++ src/app.js
@@ -4,30 +4,30 @@ import _settings  from './css/_settings.scss';
 import foundation  from './css/foundation.css';
 import css from './css/styles.css';
 
-// Import jQuery
+// Import jQuery & underscore
 import $ from 'jquery';
 import _ from 'underscore';
 import Task from './models/task';
 
-var render = function(task) {
-  // Select the template using jQuery
-  var template_text = $('#taskItemTemplate').html();
-
-  //console.log(template_text);
-  // Get an underscore template object
-  var template = _.template(template_text);
-
-  // Use the underscore template function to compile the
-  // template and data into raw html.
-  var compiledHTML = template(task.toJSON());
-
-  // append the html to the unordered list.
-  $('.todo-items').append(compiledHTML);
-};
-
 // ready to go
 $(document).ready(function() {
 
+  var render = function(task) {
+    // Select the template using jQuery
+    var template_text = $('#taskItemTemplate').html();
+
+    //console.log(template_text);
+    // Get an underscore template object
+    var template = _.template(template_text);
+
+    // Use the underscore template function to compile the
+    // template and data into raw html.
+    var compiledHTML = template(task.toJSON());
+
+    // append the html to the unordered list.
+    $('.todo-items').append(compiledHTML);
+  };
+  
   var my_task = new Task({
     title: "Create a model",
     completed: true
