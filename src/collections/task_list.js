1bbcd52e5b487d727eb5b6b5b0b465f363192772
Chris <chris@adadevelopersacademy.org>
2017-05-15 22:19:39 -0700

    added .. in taskList.

--- src/collections/task_list.js
+++ src/collections/task_list.js
@@ -1,6 +1,6 @@
 // app/collections/task_list.js
 import Backbone from 'backbone';
-import Task from 'app/models/task';
+import Task from '../models/task';
 
 var TaskList = Backbone.Collection.extend({
   model: Task
