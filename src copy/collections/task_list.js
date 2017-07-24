5d770d5b57ae0370e53ea5e08640dd32f034808f
Chris <chris@adadevelopersacademy.org>
2017-07-24 09:45:24 -0700

    Deployed.

--- src copy/collections/task_list.js
+++
@@ -1,9 +0,0 @@
-// app/collections/task_list.js
-import Backbone from 'backbone';
-import Task from '../models/task';
-
-var TaskList = Backbone.Collection.extend({
-  model: Task
-});
-
-export default TaskList;
